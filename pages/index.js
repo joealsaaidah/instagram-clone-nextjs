65ee1c9ffa951782035275eb49ece7096c55028f
joealsaaidah <alsaaidahyousef@gmail.com>
2021-12-03 18:10:39 +0800

    set up project files

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Instagram-Clone</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className=''>
+        <h1>Instagram clone</h1>
+      </main>
+    </div>
+  );
+}
